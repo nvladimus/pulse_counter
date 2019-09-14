00e89e6c97eb5051e020c8bbfbd8a8becfb8cecb
Nikita Vladimirov <nvladimus@gmail.com>
2019-09-14 19:44:17 +0200

    first commit
    Blink LED on every rising edge of digital input at pin 5.

---
+++ pulse_counter.ino
@@ -0,0 +1,33 @@
+// This code counts rising edges of an input trigger (from camera or stage) 
+// The input should be connected to Arduino's digital pin capable of Interrupt mode.
+// The program switches analog output voltage setting after every N trigger pulses
+// tested on Teensy 2.0, should work on Arduino, too.
+// Nikita Vladimirov, 2019.
+
+const byte interruptPin = 5;
+const byte ledPin = 11;
+volatile int counter = 0;
+volatile int counter_old = 0;
+volatile byte ledState = LOW;
+  
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Basic counter test:");
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), count, RISING);
+}
+
+void loop() {
+  if(counter > counter_old){
+    Serial.println(counter);
+    counter_old = counter;
+  }
+  digitalWrite(ledPin, ledState);
+}
+
+void count() {
+  counter++;
+  ledState = !ledState;
+}
+
