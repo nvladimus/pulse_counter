c34f42b5e97a5a3d2a5eca4cf80a2baaa888e3ed
Nikita Vladimirov <nvladimus@gmail.com>
2019-09-17 13:54:02 +0200

    cleaned up, added Fritzing wiring diagram
    Diagram uses Adafruit breakout board and Teensy 2.0.

--- pulse_counter.ino
+++ pulse_counter.ino
@@ -1,32 +1,37 @@
-// This code counts rising edges of an input trigger (from camera or stage) 
-// The input should be connected to Arduino's digital pin capable of Interrupt mode.
-// The program switches analog output voltage setting after every N trigger pulses
+// This code counts rising edges of an input trigger (from camera or stage) and 
+// switches analog output voltage setting after every N trigger pulses.
+// In this version, analog output is unipolar (0..5V), by using external DAC chip MCP4725 wired to Teensy via I2C. 
 // tested on Teensy 2.0, should work on Arduino, too.
-// DAC chip MCP4725 wired via I2C. 
 // Default speed of Arduino Wire library is 100KHz. The Adafruit MCP4725 library can run at speed 400KHz by setting the TWBR = 12;
 // Nikita Vladimirov, 2019.
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
 
-const float VCC = 5.0; // power supply voltage
-const byte interruptPin = 7; // Teensy 2.0, interrupt pins are: 5, 6, 7, 8.
-const byte ledPin = 11;
-volatile int counter = 0;
-volatile int counter_old = 0;
-volatile byte ledState = LOW;
+const float VCC = 5.0; // power supply voltage. Can be 3.3V as well.
+const byte interruptPin = 7; // Teensy 2.0, interrupt pins are: 5, 6, 7, 8. This digital pin must be capable of Interrupt mode.
+const byte ledPin = 11; // blink every time a pulse is detected
+
+volatile int counter;
+volatile int counter_old;
+volatile byte ledState;
 Adafruit_MCP4725 dac; // constructor
 const float voltage_out_0 = 0.5;
 const float voltage_out_1 = 4.5;
 const int n_pulses_switch_period = 10; // DAC output will alternate between voltage_out_0 and voltage_out_1 with this period
-uint16_t dac_value = 0; 
-uint16_t dac_value_0 = 0; 
-uint16_t dac_value_1 = 0; 
+uint16_t dac_value; 
+uint16_t dac_value_0; 
+uint16_t dac_value_1; 
 
 void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
+  ledState = LOW;
+  
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), count, RISING);
+  counter = 0;
+  counter_old = 0;
+  
   dac.begin(0x62); // if unsure, use I2C scanner to find the I2C address of your MCP4725: http://henrysbench.capnfatz.com/henrys-bench/arduino-projects-tips-and-more/arduino-quick-tip-find-your-i2c-address/
   dac_value_0 = volts2dac_value(voltage_out_0);
   dac_value_1 = volts2dac_value(voltage_out_1);
